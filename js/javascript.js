11d3e546fdce2909651a835a9ae293a7fdca6492
JeimyCoronado <jeimycoronado100@gmail.com>
2022-08-18 15:13:14 -0500

    seccion contacto

--- js/javascript.js
+++ js/javascript.js
@@ -3,23 +3,54 @@
     const btnNosotros = document.querySelector("[data-btn-nosotros]");
     const divHome = document.querySelector("[data-home]");
     const divNosotros = document.querySelector("[data-nosotros]");
+    const btnProductos = document.querySelector("[data-btn-productos]");
+    const divProductos = document.querySelector("[data-productos]");
+    const btnContacto = document.querySelector("[data-btn-contacto]");
+    const divContacto = document.querySelector("[data-contacto]");
 
-    const seleccionarHome = (event) => {
+    const divs = [divHome, divNosotros, divProductos, divContacto];
+    const btns = [btnHome, btnNosotros, btnProductos, btnContacto];
+
+    const mostrarSeccion = (event) => {
         const boton = event.target;
-        if(boton == btnHome) {
-            btnHome.classList.add("head-enlace-btn-seleccionado"); 
-            divHome.classList.remove("ocultar");
-            btnNosotros.classList.remove("head-enlace-btn-seleccionado");
-            divNosotros.classList.add("ocultar");
 
-        }else if(boton == btnNosotros) {
-            btnNosotros.classList.add("head-enlace-btn-seleccionado");
-            divNosotros.classList.remove("ocultar");
-            btnHome.classList.remove("head-enlace-btn-seleccionado");
-            divHome.classList.add("ocultar");
+        for(let i=0; i<btns.length; i++) {
+            if(btns[i] == event.target) {
+                btns[i].classList.add("head-enlace-btn-seleccionado");
+                divs[i].classList.remove("ocultar");
+            }
+            else{
+                btns[i].classList.remove("head-enlace-btn-seleccionado");
+                divs[i].classList.add("ocultar")
+            }
         }
+
+
+        // divNosotros.classList.add("ocultar");
+        // divHome.classList.add("ocultar");
+        // divProductos.classList.add("ocultar");
+        // divContacto.classList.add("ocultar");
+        // btnHome.classList.remove("head-enlace-btn-seleccionado");
+        // btnNosotros.classList.remove("head-enlace-btn-seleccionado");
+        // btnProductos.classList.remove("head-enlace-btn-seleccionado");
+        // btnContacto.classList.remove("head-enlace-btn-seleccionado");
+
+        // if(boton == btnHome) {
+        //     btnHome.classList.add("head-enlace-btn-seleccionado"); 
+        //     divHome.classList.remove("ocultar");
+        // }else if(boton == btnNosotros) {
+        //     btnNosotros.classList.add("head-enlace-btn-seleccionado");
+        //     divNosotros.classList.remove("ocultar");
+        // }else if(boton == btnProductos) {
+        //     btnProductos.classList.add("head-enlace-btn-seleccionado");
+        //     divProductos.classList.remove("ocultar");
+        // }else {
+        //     btnContacto.classList.add("head-enlace-btn-seleccionado");
+        //     divContacto.classList.remove("ocultar");
+        // }
     }
 
-    btnHome.addEventListener("click", seleccionarHome);
-    btnNosotros.addEventListener("click", seleccionarHome);
+    btnHome.addEventListener("click", mostrarSeccion);
+    btnNosotros.addEventListener("click", mostrarSeccion);
+    btnContacto.addEventListener("click", mostrarSeccion);
 })();
